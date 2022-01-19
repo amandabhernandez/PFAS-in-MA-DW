d24a6b54045e3fc310e427d0589bd82ff9c88e73
amandabhernandez <amandabhernandez@gmail.com>
2022-01-19 09:21:10 -0600

    add comments throughout code

--- ui.R
+++ ui.R
@@ -9,46 +9,50 @@ library(plotly)
 library(tidyverse)
 library(lubridate)
 
-
+# set up page structure 
 shinyUI(navbarPage("PFAS in MA Drinking Water", 
                    tabPanel("What's in my water?", 
                             fluidPage(
-                                theme = shinytheme("cerulean"),
-                                #tags$head(includeHTML(("html/google-analytics.html"))),
-                                fluidRow(
-                                    column(5,
-                                           wellPanel(h3("What are PFAS?"),
-                                                     htmlOutput("pfas_exp"),
-                                                     h3("Health Effects"),
-                                                     htmlOutput("health_effects"),
-                                                     h3("Are there PFAS in my water?"),
-                                                     htmlOutput("summary"),
-                                                     h3("What can I do?"),
-                                                     p("There are many steps you can take to reduce your exposure to PFAS."),
-                                                     h4("Water Treatment"),
-                                                     htmlOutput("treatment"),
-                                                     h4("In Your Community"),
-                                                     htmlOutput("community"))
-                                    ),
-                                    column(7,
-                                           h2("My PFAS Water Report"),
-                                           uiOutput("instructions"),
-                                           h4(""),
-                                           fluidRow(column(6,uiOutput("town"),
-                                                           uiOutput("chemicals")),
-                                                    column(2, uiOutput("year"),
-                                                           actionButton("download", 
-                                                                        "Download Report", 
-                                                                        icon = icon("download")))),
-                                           
-                                           tabsetPanel(
-                                             tabPanel("Graphs", 
-                                                      p(""),
-                                                      htmlOutput("hint"),
-                                                      plotlyOutput("dw")),
-                                             tabPanel("Table", DT::dataTableOutput("dw_table")))
-                                    )
+                              #set theme
+                              theme = shinytheme("cerulean"),
+                              #add analytics
+                              tags$head(includeHTML(("html/google-analytics.html"))),
+                              fluidRow(
+                                column(5,
+                                       #add panel with text on the left side of the page
+                                       wellPanel(h3("What are PFAS?"),
+                                                 htmlOutput("pfas_exp"),
+                                                 h3("Health Effects"),
+                                                 htmlOutput("health_effects"),
+                                                 h3("Are there PFAS in my water?"),
+                                                 htmlOutput("summary"),
+                                                 h3("What can I do?"),
+                                                 p("There are many steps you can take to reduce your exposure to PFAS."),
+                                                 h4("Water Treatment"),
+                                                 htmlOutput("treatment"),
+                                                 h4("In Your Community"),
+                                                 htmlOutput("community"))
+                                ),
+                                #add input options 
+                                column(7,
+                                       h2("My PFAS Water Report"),
+                                       uiOutput("instructions"),
+                                       h4(""),
+                                       fluidRow(column(6,uiOutput("town"),
+                                                       uiOutput("chemicals")),
+                                                column(2, uiOutput("year"),
+                                                       #button does not currently work, requires Rmd file 
+                                                       actionButton("download", 
+                                                                    "Download Report", 
+                                                                    icon = icon("download")))),
+                                       #add graph and table content 
+                                       tabsetPanel(
+                                         tabPanel("Graphs",
+                                                  htmlOutput("hint"),
+                                                  plotlyOutput("dw")),
+                                         tabPanel("Table", DT::dataTableOutput("dw_table")))
                                 )
+                              )
                             )
                             
                    ),
